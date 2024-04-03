5d1587992043d7873c84455dcff19dcc83881f66
Laxman Shrestha <120126198+LaxmanShr2003@users.noreply.github.com>
2024-04-03 12:59:44 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Laxman Shrestha",
+  title: "Web Developer",
+  email: "shrlaxman9861@gmail.com",
+  gitHub: "https://github.com/LaxmanShr2003",
+  instagram: "https://www.instagram.com/acrylic_alexzen/",
+  linkedIn: "www.linkedin.com/in/laxman-shrestha-9619ba230",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
